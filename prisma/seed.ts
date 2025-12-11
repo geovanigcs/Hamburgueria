2b157384543e818cd0e11437dca50efa19754828
Geovani <geovanigcs.dev@gmail.com>
2025-12-11 12:57:27 -0300

    feat: atualiza seed com imagens locais e corrige página inicial para redirecionar para /gigios

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -12,10 +12,8 @@ const main = async () => {
         name: "Gigio's Delivery",
         slug: "gigios",
         description: "O melhor fast food do mundo",
-        avatarImageUrl:
-          "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQvcNP9rHlEJu1vCY5kLqzjf29HKaeN78Z6pRy",
-        coverImageUrl:
-          "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQU3JZGQeTmvPeJLoyOjzNsMqFdxUI423nBl6b",
+        avatarImageUrl: "/logoavatar.png",
+        coverImageUrl: "/salãohamburgeria.png",
       },
     });
     const combosCategory = await tx.menuCategory.create({
@@ -31,8 +29,7 @@ const main = async () => {
           description:
             "Quatro hambúrgueres (100% carne bovina), alface americana, queijo fatiado sabor cheddar, molho especial, cebola, picles e pão com gergilim, acompanhamento e bebida.",
           price: 39.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQaHB8tslkBUjlHSKiuseLm2hIFzVY0OtxEPnw",
+          imageUrl: "/McOferta Média Big Mac Duplo.png",
           menuCategoryId: combosCategory.id,
           restaurantId: restaurant.id,
           ingredients: [
@@ -50,8 +47,7 @@ const main = async () => {
           description:
             "Dois hambúrgueres de carne 100% bovina, méquinese, a exclusiva maionese especial com sabor de carne defumada, onion rings, fatias de bacon, queijo processado sabor cheddar, o delicioso molho lácteo com queijo tipo cheddar tudo isso no pão tipo brioche trazendo uma explosão de sabores pros seus dias de glória! Acompanhamento e Bebida.",
           price: 41.5,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQeGQofnEPyQaHEV2WL8rGUs41oMICtYfNkphl",
+          imageUrl: "/Novo Brabo Melt Onion Rings.png",
           menuCategoryId: combosCategory.id,
           restaurantId: restaurant.id,
           ingredients: [
@@ -70,8 +66,7 @@ const main = async () => {
           description:
             "Composto por pão tipo brioche com batata, molho Honey&Fire, bacon em fatias, alface, tomate, queijo sabor cheddar e carne 100% de peito de frango, temperada e empanada, acompanhamento e bebida.",
           price: 39.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQr12aTqPo3SsGjBJCaM7yhxnbDlXeL5N9dckv",
+          imageUrl: "/McCrispy Chicken Elite.png",
           menuCategoryId: combosCategory.id,
           restaurantId: restaurant.id,
           ingredients: [
@@ -90,8 +85,7 @@ const main = async () => {
           description:
             "Dois hambúrgueres (100% carne bovina), molho lácteo com queijo tipo cheddar, cebola ao molho shoyu e pão escuro com gergelim, acompanhamento e bebida.",
           price: 36.2,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQWdq0w8niS9XCLQu7Nb4jvBYZze16goaOqsKR",
+          imageUrl: "/Duplo Cheddar McMelt.png",
           menuCategoryId: combosCategory.id,
           restaurantId: restaurant.id,
           ingredients: [
@@ -125,8 +119,7 @@ const main = async () => {
             "Picles",
           ],
           price: 39.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQKfI6fivqActTvBGLXfQe4a8CJ6d3HiR7USPK",
+          imageUrl: "/combo1.png",
           menuCategoryId: hamburguersCategory.id,
           restaurantId: restaurant.id,
         },
@@ -145,8 +138,7 @@ const main = async () => {
             "Molho lácteo com queijo tipo cheddar",
           ],
           price: 41.5,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQ99rtECuYaDgmA4VujBU0wKn2ThXJvF3LHfyc",
+          imageUrl: "/combo2.png",
           menuCategoryId: hamburguersCategory.id,
           restaurantId: restaurant.id,
         },
@@ -165,8 +157,7 @@ const main = async () => {
             "Carne 100% de peito de frango",
           ],
           price: 39.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQUY0VlDTmvPeJLoyOjzNsMqFdxUI423nBl6br",
+          imageUrl: "/combo3.png",
           menuCategoryId: hamburguersCategory.id,
           restaurantId: restaurant.id,
         },
@@ -181,8 +172,7 @@ const main = async () => {
             "Cebola ao molho shoyu",
           ],
           price: 36.2,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQBBmifbjzEVXRoycAtrP9vH45bZ6WDl3QF0a1",
+          imageUrl: "/combo4.png",
           menuCategoryId: hamburguersCategory.id,
           restaurantId: restaurant.id,
         },
@@ -201,8 +191,7 @@ const main = async () => {
           description: "Batatas fritas crocantes e sequinhas. Vem bastante!",
           ingredients: [],
           price: 10.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQNd3jSNrcJroaszwjUAlM6iSO5ZTx2HV70t31",
+          imageUrl: "/Fritas.png",
           menuCategoryId: frenchFriesCategory.id,
           restaurantId: restaurant.id,
         },
@@ -212,8 +201,7 @@ const main = async () => {
             "Batatas fritas crocantes e sequinhas. Vem uma média quantidade!",
           ingredients: [],
           price: 9.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQ7Y6lv9tkc0L9oMIXZsFJtwnBh2KCz3y6uSW1",
+          imageUrl: "/Fritas-1.png",
           menuCategoryId: frenchFriesCategory.id,
           restaurantId: restaurant.id,
         },
@@ -223,8 +211,7 @@ const main = async () => {
             "Batatas fritas crocantes e sequinhas. Vem pouquinho (é bom pra sua dieta)!",
           ingredients: [],
           price: 5.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQ5toOZxYa1oARJCUGh4EY3x8NjXHtvZ7lnVfw",
+          imageUrl: "/batatas 1.png",
           menuCategoryId: frenchFriesCategory.id,
           restaurantId: restaurant.id,
         },
@@ -243,8 +230,7 @@ const main = async () => {
           description: "Coca-cola gelada para acompanhar seu lanche.",
           ingredients: [],
           price: 5.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQJS1b33q29eEsh0CVmOywrqx1UPnJpRGcHN5v",
+          imageUrl: "/image 1.png",
           menuCategoryId: drinksCategory.id,
           restaurantId: restaurant.id,
         },
@@ -253,8 +239,7 @@ const main = async () => {
           description: "Fanta Laranja gelada para acompanhar seu lanche.",
           ingredients: [],
           price: 5.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQW7Kxm9gniS9XCLQu7Nb4jvBYZze16goaOqsK",
+          imageUrl: "/image 2.png",
           menuCategoryId: drinksCategory.id,
           restaurantId: restaurant.id,
         },
@@ -263,8 +248,7 @@ const main = async () => {
           description: "A bebida favorita do Cristiano Ronaldo.",
           ingredients: [],
           price: 2.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQ7i05S5tkc0L9oMIXZsFJtwnBh2KCz3y6uSW1",
+          imageUrl: "/image 5.png",
           menuCategoryId: drinksCategory.id,
           restaurantId: restaurant.id,
         },
@@ -283,8 +267,7 @@ const main = async () => {
           description: "Casquinha de sorvete sabor baunilha.",
           ingredients: [],
           price: 3.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQtfuQrAKkI75oJfPT0crZxvX82ui9qV3hLFdY",
+          imageUrl: "/sorvetes.png",
           menuCategoryId: desertsCategory.id,
           restaurantId: restaurant.id,
         },
@@ -293,8 +276,7 @@ const main = async () => {
           description: "Casquinha de sorvete sabor chocolate.",
           ingredients: [],
           price: 3.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQBH21ijzEVXRoycAtrP9vH45bZ6WDl3QF0a1M",
+          imageUrl: "/sorvetinho.png",
           menuCategoryId: desertsCategory.id,
           restaurantId: restaurant.id,
         },
@@ -303,8 +285,7 @@ const main = async () => {
           description: "Casquinha de sorvete sabor baunilha e chocolate.",
           ingredients: [],
           price: 2.9,
-          imageUrl:
-            "https://u9a6wmr3as.ufs.sh/f/jppBrbk0cChQ4rBrtULypXmR6JiWuhzS8ALjVkrF3yfatC7E",
+          imageUrl: "/sorvetes_sandubas.png",
           menuCategoryId: desertsCategory.id,
           restaurantId: restaurant.id,
         },
